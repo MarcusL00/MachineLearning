2e2a5e86cf390149fedf51234cdbf3918f527202
MarcusL00 <marcuswind123@gmail.com>
2025-11-12 10:39:07 +0100

    Cleans up imports in prediction route handler
    Removes unnecessary and duplicate imports to simplify dependencies
    and improve code readability. Streamlines import statements to only
    include used modules.

--- api/app/routes/predictionRoute.py
+++ api/app/routes/predictionRoute.py
@@ -1,13 +1,12 @@
 
 from app import app
-from flask import request
-from flask import abort, app
+from flask import request, abort
 
 from app.utilities.cleanCSV import cleanCSVData
+
 @app.route("/prediction", methods=["POST"])
 
 def make_prediction():
-    import app.utilities
     data = request
     csv_file = data.files.get("csv_file")
     
